3a0bf3e812e8b2d2eaf2042d05fd023d8bf4acfa
GabrielNew <gabrielfai70@gmail.com>
2024-10-09 21:10:25 -0300

    Update frontend

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,25 +1,21 @@
 import { useState } from "react";
-import "../../index.css";
+import "./styles.css";
 
 const Home = () => {
   const [city, setCity] = useState("");
-
-  // const KEY = dotenv.WEATHER_API_KEY;
-  // const BASE_URL = dotenv.WEATHER_API_URL;
-  //"http://api.weatherapi.com/v1/current.json?key=" + KEY + "&q=";
 
   const onChange = (event) => {
     setCity(event.target.value);
   };
 
   async function getData() {
-    const test = import.meta.env.VITE_WEATHER_API_URL.concat(
+    const url = import.meta.env.VITE_WEATHER_API_URL.concat(
       import.meta.env.VITE_WEATHER_API_KEY,
       "&q=",
       city
     );
     try {
-      const response = await fetch(test);
+      const response = await fetch(url);
 
       if (!response.ok) {
         throw new Error(`Response status: ${response.status}`);
@@ -33,26 +29,24 @@ const Home = () => {
   }
 
   return (
-    <div>
-      <header>
-        <div>
-          <p>WeatherApp.com</p>
-        </div>
-        <div>
-          <input
-            type="text"
-            name="cityInput"
-            id="cityInput"
-            placeholder="What city do you want?"
-            value={city}
-            onChange={onChange}
-          />
-          <button type="button" onClick={getData}>
-            Search
-          </button>
-        </div>
-      </header>
-    </div>
+    <header>
+      <div id="Title">
+        <h3>WeatherApp.com</h3>
+      </div>
+      <div id="UserInput">
+        <input
+          type="text"
+          name="cityInput"
+          id="cityInput"
+          placeholder="What city do you want?"
+          value={city}
+          onChange={onChange}
+        />
+        <button type="button" onClick={getData}>
+          Search
+        </button>
+      </div>
+    </header>
   );
 };
 
