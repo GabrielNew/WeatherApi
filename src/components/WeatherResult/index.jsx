c8f2ebd2e5af256afe93a700005c73654b4be6d1
GabrielNew <gabrielfai70@gmail.com>
2024-10-12 21:43:43 -0300

    Text correction

--- src/components/WeatherResult/index.jsx
+++ src/components/WeatherResult/index.jsx
@@ -12,8 +12,8 @@ const WeatherResult = ({ weatherData }) => {
       <p>Country: {weatherData.location.country}</p>
       <p>Localtime: {weatherData.location.localtime}</p>
       <p>Last update: {weatherData.current.last_updated}</p>
-      <p>Temperature in celcius: {weatherData.current.temp_c}</p>
-      <p>Temperature in fareinheit: {weatherData.current.temp_f}</p>
+      <p>Temperature in Celcius: {weatherData.current.temp_c}</p>
+      <p>Temperature in Fahrenheit: {weatherData.current.temp_f}</p>
       <p>
         Condition: {weatherData.current.condition.text}
         <img id="WeatherIcon" src={weatherData.current.condition.icon}></img>
